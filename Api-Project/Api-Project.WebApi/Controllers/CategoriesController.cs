e47e20fc6dc6308c60aa39d1a2412dcfae8691d2
kaansarr <kaansar33@gmail.com>
2025-07-03 20:00:27 +0300

    Up
    Kategori ve Şef listesinin api controller yazımı

---
+++ Api-Project/Api-Project.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+﻿using Api_Project.WebApi.Context;
+using Api_Project.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Project.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ApiContext _context;
+		public CategoriesController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult CategoryList()
+		{
+			var values = _context.Categories.ToList();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateCategory(Category category) 
+		{
+			_context.Categories.Add(category);
+			_context.SaveChanges();
+			return Ok("Kategori ekleme işlemi başarılı");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteCategory(int id) 
+		{
+			var values = _context.Categories.Find(id);
+			_context.Categories.Remove(values);
+			_context.SaveChanges();
+			return Ok("Kategori Silme İşlemi başarılı");
+		}
+
+
+		[HttpGet("GetCategory")]
+		public IActionResult GetCategory(int id) 
+		{
+			var values = _context.Categories.Find(id);
+			return Ok(values);
+		}
+
+		[HttpPut]
+		public IActionResult PutCategory(Category category) 
+		{ 
+			_context.Categories.Update(category);
+			_context.SaveChanges();
+			return Ok("Güncelleme işlemi başarılı");
+		}
+
+	}
+}
---
+++ Api-Project/Api-Project.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,62 @@
+﻿using Api_Project.WebApi.Context;
+using Api_Project.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Project.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ChefsController : ControllerBase
+	{
+		private readonly ApiContext _context;
+
+		public ChefsController(ApiContext context)
+		{
+			_context = context;
+		}
+
+
+		[HttpGet]
+		public IActionResult ChefList() 
+		{ 
+			var values=_context.Chefs.ToList();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateChef(Chef chef)
+		{
+			_context.Chefs.Add(chef);
+			_context.SaveChanges();
+			return Ok("Şef Sisteme eklendi");
+		}
+
+
+		[HttpDelete]
+		public IActionResult DeleteChef(int id)
+		{
+			var values=_context.Chefs.Find(id);
+			_context.Chefs.Remove(values);
+			_context.SaveChanges();
+			return Ok("Şef sistemden silindi");
+
+		}
+
+		[HttpGet("GetChef")]
+		public IActionResult GetChef(int id) 
+		{ 
+			var values= _context.Chefs.Find(id);
+			return Ok(values);
+		}
+
+
+		[HttpPut]
+		public IActionResult UpdateChef(Chef chef)
+		{
+			_context.Chefs.Update(chef);
+			_context.SaveChanges();
+			return Ok("Şef Güncellendi");
+		}
+	}
+}
