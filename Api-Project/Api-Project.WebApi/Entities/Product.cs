7968c5875319f6c87375c52f3cc35aa2e4962ec2
kaansarr <kaansar33@gmail.com>
2025-07-03 18:52:17 +0300

    Proje başlangıcı
    Proje başlangıcı ve Entitylerin oluşturulması

---
+++ Api-Project/Api-Project.WebApi/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Api_Project.WebApi.Entities
+{
+	public class Product
+	{
+		public int ProductId { get; set; }
+		public string ProductName { get; set; }
+
+		public string ProductDescription { get; set; }
+		public decimal Price { get; set; }
+		public decimal ImageUrl { get; set; }
+	}
+}
